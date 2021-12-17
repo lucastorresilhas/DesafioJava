d8cdfe050eec0b90125d5937814838800b70c0e8
Lucas Gomes Torresilhas <lucas.torresilhas@sp01.local>
2021-12-17 15:33:28 -0300

    Classes fazenda e endereço criadas

---
+++ src/main/java/br/com/totvs/desafio/models/Endereco.java
@@ -0,0 +1,30 @@
+package br.com.totvs.desafio.models;
+
+public class Endereco {
+
+	private String cidade;
+	private String estado;
+	private String logradouro;
+	
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	
+	
+}
+
---
+++ src/main/java/br/com/totvs/desafio/models/Fazenda.java
@@ -0,0 +1,29 @@
+package br.com.totvs.desafio.models;
+
+public class Fazenda {
+
+	private String nome;
+	private String cnpj;
+	private Endereco endereco;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+}
